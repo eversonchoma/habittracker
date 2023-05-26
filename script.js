0bc1ce8d37f4ad15af94d4dbad44d2579f2c74f0
everson.choma <everson.choma@gmail.com>
2023-05-25 22:08:52 -0300

    feat: Adding the Add and Save functionalities

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "02-01", "05-01"],
-  music: ["01-01", "03-01", "06-01", "07-01", "08-01", "09-01"],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  if (nlwSetup.dayExists(today)) {
+    alert("Day already added")
+    return
+  }
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
